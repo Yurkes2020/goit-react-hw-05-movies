f3ef9fba8846f9fdc117464b8a4e4c79ea490814
Yurkes2020 <yurkes2020@gmail.com>
2022-11-27 20:27:45 +0200

    fix

--- src/components/Cast/Cast.styled.jsx
+++ src/components/Cast/Cast.styled.jsx
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const List = styled.ul`
   display: grid;
   grid-gap: 10px;
-  grid-template-columns: 2fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
 `;
